147acb27ddec722f456c08d04c10e7e4d603c7d0
Vimanshu Aryan <vimanshuaryan@gmail.com>
2023-05-08 12:09:30 +0530

    08-05-23

--- duplicate.js
+++ duplicate.js
@@ -1,8 +1,22 @@
 //217 
-nums = [1,2,3,1]
+nums2 = [1,2,3,5]
 function dupli (nums) {
     const s = new Set(nums)
     return s.size !== nums.length
 };
 
-console.log(dupli(nums))
+console.log(dupli(nums2))
+
+nums = [2,14,18,22,22]
+var dupli2 = function(nums) {
+    nums.sort((a,b) => a-b);
+    for (let i=0; i<= nums.length-1; i++) {
+        console.log(i, i+1)
+        if (nums[i] === nums[i+1]) {
+            return true
+        }
+
+    }
+    return false
+};
+console.log(dupli2(nums))
